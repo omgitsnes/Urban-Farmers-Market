28317a222e411dac15e78eb7913a864b1722ba43
Fernando Silva <worldme_13@hotmail.com>
2016-06-23 04:24:02 +0100

    sidebar close button now is black

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -36,7 +36,7 @@
     <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
     <nav id="sidebar-wrapper">
         <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+            <a id="menu-close" href="#" class="btn btn-dark btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
             <li class="sidebar-brand">
                 <a href="#top"  onclick = $("#menu-close").click(); >Urban Farmer's Market</a>
             </li>
