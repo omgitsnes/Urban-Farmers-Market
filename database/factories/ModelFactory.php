a292e78238ed43dba826be193ffd9076c54e9e8a
Fernando Silva <worldme_13@hotmail.com>
2016-06-29 00:21:59 +0100

    Fake user generator

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -27,7 +27,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
         'location' => $faker->city,
         'presentation' => $faker->paragraph(3, true),
+        'blocked' => rand(0, 1),
+        'sells_evals' => rand(1, 5),
+        'sells_count' => rand(0, 1000),
+        'buys_evals' => rand(1, 5),
+        'buys_count' => rand(0, 1000),
+        'profile_photo' => imageUrl($width = 640, $height = 480)
+    ];
+});
 
+$factory->define(App\Advertisement::class, function (Faker\Generator $faker) {
 
+    $faker->addProvider(new Faker\Provider\en_US\Person($faker));
+    $faker->addProvider(new Faker\Provider\en_US\Address($faker));
+    $faker->addProvider(new Faker\Provider\en_US\PhoneNumber($faker));
+    $faker->addProvider(new Faker\Provider\en_US\Company($faker));
+    $faker->addProvider(new Faker\Provider\Lorem($faker));
+    $faker->addProvider(new Faker\Provider\Internet($faker));
+
+    return [
+        'owner_id',
+        'name',
+        'description',
+        'price_cents',
+        'trade_prefs',
+        'quantity',
+        'blocked',
     ];
 });
